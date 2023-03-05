7e491657d41b092c1e331bcb8bdf54a9e2a13108
Halit Uzan <halit.uzan@gmail.com>
2023-03-06 00:32:12 +0300

    samesite

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var session = require('express-session')
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
@@ -7,6 +8,23 @@ const authRoutes = require("./Routes/auth.routes");
 const crudRoutes = require("./Routes/crud.routes");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const KnexSessionStore = require('connect-session-knex')(session);
+const store = new KnexSessionStore({
+  tablename: 'session',
+  knex: kx,
+  createtable: false
+});
+
+const sessionConfig = {
+  secret: 'ty hb entegrenity com',
+  name: 'Entegrenity',
+  store: store,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: 'none', // THIS is the config you are looing for.
+  }
+};
 
 app.listen(PORT, () => {
   console.log("server Started on PORT", PORT);
@@ -34,6 +52,12 @@ app.use(
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  sessionConfig.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionConfig))
 
 app.use(cookieParser());
 app.use(express.json());
